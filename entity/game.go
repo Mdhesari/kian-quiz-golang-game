dc7d2f7abdc96cc7b5833fadc94fa685f07ee24a
Mohamad Hesari <mdhesari99@gmail.com>
2024-12-19 16:10:44 +0330

    feat: game migration

--- entity/game.go
+++ entity/game.go
@@ -6,11 +6,20 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type Round struct {
+	QuestionId primitive.ObjectID `bson:"question_id"`
+	PlayerId   primitive.ObjectID `bson:"player_id"`
+	Answer     Answer             `bson:"answer"`
+	StartTime  time.Time          `bson:"start_time"`
+	EndTime    time.Time          `bson:"end_time"`
+}
+
 type Game struct {
 	ID          primitive.ObjectID   `bson:"_id,omitempty"`
 	CategoryID  primitive.ObjectID   `bson:"category_id"`
 	QuestionIDs []primitive.ObjectID `bson:"question_ids"`
 	PlayerIDs   []primitive.ObjectID `bson:"player_ids"`
+	Rounds      []Round              `bson:"rounds"`
 	WinnerID    primitive.ObjectID   `bson:"winner_id"`
 	StartTime   time.Time            `bson:"start_time"`
 	ExpiresTime time.Time            `bson:"expires_time"`

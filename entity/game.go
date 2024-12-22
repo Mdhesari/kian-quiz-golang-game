40c13570fcdd3c07c29f6bdb9cbbdb749673198b
Mohamad Hesari <mdhesari99@gmail.com>
2024-12-22 13:39:38 +0330

    refactor: update game and player entities

--- entity/game.go
+++ entity/game.go
@@ -6,21 +6,14 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type PlayerAnswer struct {
-	QuestionId primitive.ObjectID `bson:"question_id"`
-	PlayerId   primitive.ObjectID `bson:"player_id"`
-	Answer     Answer             `bson:"answer"`
-	StartTime  time.Time          `bson:"start_time"`
-	EndTime    time.Time          `bson:"end_time"`
-}
-
 type Game struct {
 	ID             primitive.ObjectID   `bson:"_id,omitempty"`
 	CategoryID     primitive.ObjectID   `bson:"category_id"`
 	QuestionIDs    []primitive.ObjectID `bson:"question_ids"`
-	PlayerIDs      []primitive.ObjectID `bson:"player_ids"`
-	PlayersAnswers []PlayerAnswer       `bson:"player_answers"`
-	WinnerID       primitive.ObjectID   `bson:"winner_id"`
+	PlayerIds      []primitive.ObjectID `bson:"player_ids"`
+	WinnerPlayerID primitive.ObjectID   `bson:"winner_player_id"`
 	StartTime      time.Time            `bson:"start_time"`
-	ExpiresTime    time.Time            `bson:"expires_time"`
+	EndTime        time.Time            `bson:"end_time"`
+	CreatedAt      time.Time            `bson:"created_at"`
+	UpdatedAt      time.Time            `bson:"updated_at"`
 }
--- entity/player.go
+++ entity/player.go
@@ -1,11 +1,25 @@
 package entity
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type PlayerAnswer struct {
+	QuestionId primitive.ObjectID `bson:"question_id"`
+	Answer     Answer             `bson:"answer"`
+	Score      int                `bson:"score"`
+	StartTime  time.Time          `bson:"start_time"`
+	EndTime    time.Time          `bson:"end_time"`
+}
 
 type Player struct {
-	ID        primitive.ObjectID   `bson:"_id,omitempty"`
-	UserID    primitive.ObjectID   `bson:"user_id"`
-	GameID    primitive.ObjectID   `bson:"game_id"`
-	AnswerIDs []primitive.ObjectID `bson:"answer_ids"`
-	Score     int                  `bson:"score"`
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	UserID    primitive.ObjectID `bson:"user_id"`
+	GameID    primitive.ObjectID `bson:"game_id"`
+	Answers   []PlayerAnswer     `bson:"answers"`
+	Score     int                `bson:"score"`
+	CreatedAt time.Time          `bson:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at"`
 }

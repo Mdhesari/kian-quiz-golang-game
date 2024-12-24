af9382e88e53071fb6ff794f0bc0f1adee414d9c
Mohamad Hesari <mdhesari99@gmail.com>
2024-12-24 06:24:48 -0500

    refactor: category entity

--- entity/category.go
+++ entity/category.go
@@ -3,7 +3,7 @@ package entity
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Category struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty"`
-	Title       string             `bson:"title"`
-	Description string             `bson:"description"`
+	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Title       string             `json:"title" bson:"title"`
+	Description string             `json:"description" bson:"description"`
 }

65e29949428d429021cb6b01eb829f09bb6799a5
Mohamad Hesari <mdhesari99@gmail.com>
2024-12-24 04:56:38 -0500

    feat: login response add user obj

--- param/login.go
+++ param/login.go
@@ -1,10 +1,13 @@
 package param
 
+import "mdhesari/kian-quiz-golang-game/entity"
+
 type LoginRequest struct {
 	Email    string `json:"email" form:"email"`
 	Password string `json:"password" form:"password"`
 }
 
 type LoginResponse struct {
-	Token  string   `json:"token"`
+	User  entity.User `json:"user"`
+	Token string      `json:"token"`
 }
